a4617f871cbfebfb8993f47da583da9ea918b4ef
wyzlhf <haifeng0130@126.com>
2023-03-21 18:13:30 +0800

    文本下载还没完成，暂时不想写了

---
+++ get_Bible_text/get_Bible_text.py
@@ -0,0 +1,105 @@
+# 从 http://www.godcom.net/ 获取圣经文本
+from typing import List, Dict
+
+import requests
+from bs4 import BeautifulSoup, ResultSet
+from docx import Document
+
+# def get_version_index_url(version: str = 'niv') -> str:
+#     print('开始生成版本链接')
+#     return f'http://www.godcom.net/{version}/index.htm'
+#
+#
+# def get_Bible_chapters(index_url: str = 'http://www.godcom.net/niv/index.htm') -> List[Dict]:
+#     print('正在获取圣经文本结构')
+#     # 文本结构  Bible_content_structure=[
+#     # {'chapter_title':'Genesis',
+#     # 'paragraph_links_list':['http://www.godcom.net/niv/B01C001.htm','http://www.godcom.net/niv/B01C002.htm']},
+#     # {'chapter_title':'Genesis',
+#     # 'paragraph_links_list':['http://www.godcom.net/niv/B01C001.htm','http://www.godcom.net/niv/B01C002.htm']},
+#     # ]
+#     Bible_content_structure: List[Dict] = []
+#     r: str = requests.get(index_url).text
+#     soup: BeautifulSoup = BeautifulSoup(r, features="lxml")
+#     # 每个tr包含圣经一章和若干个小节
+#     all_tr_tag: ResultSet = soup.find_all('tr')
+#     for tr_tag in all_tr_tag:
+#         one_chapter_structure: dict = {}
+#         all_td_in_tr_tag: ResultSet = tr_tag.find_all('td')
+#         chapter_title_td: ResultSet = all_td_in_tr_tag[0]
+#         paragraph_links_td: ResultSet = all_td_in_tr_tag[1]
+#         chapter_title: str = chapter_title_td.p.text
+#         # print(chapter_title)
+#         one_chapter_structure['chapter_title'] = chapter_title
+#         one_chapter_paragraph_links_list: list = []
+#         all_a_in_tr_tag: ResultSet = paragraph_links_td.find_all('a')
+#         for link in all_a_in_tr_tag:
+#             paragraph_link: str = 'http://www.godcom.net/niv/' + link.get('href')
+#             one_chapter_paragraph_links_list.append(paragraph_link)
+#             # print(link.get('href'))
+#         one_chapter_structure['paragraph_links_list'] = one_chapter_paragraph_links_list
+#         Bible_content_structure.append(one_chapter_structure)
+#     # print('获取圣经文本结构完成')
+#     return Bible_content_structure
+def get_Bible_chapters(index_url: str = 'http://www.godcom.net/niv/index.htm') -> List[Dict]:
+    print('正在获取圣经文本结构')
+    # 文本结构
+    # Bible_content_structure = [
+    #     {'chapter_title': 'Genesis', 'paragraph_links_list': [{'1': 'http://www.godcom.net/niv/B01C001.htm'},
+    #                                                           {'2': 'http://www.godcom.net/niv/B01C002.htm'}]},
+    #     {'chapter_title': 'Genesis', 'paragraph_links_list': [{'1': 'http://www.godcom.net/niv/B01C001.htm'},
+    #                                                           {'2': 'http://www.godcom.net/niv/B01C002.htm'}]},
+    # ]
+    Bible_content_structure: List[Dict] = []
+    r: str = requests.get(index_url).text
+    soup: BeautifulSoup = BeautifulSoup(r, features="lxml")
+    # 每个tr包含圣经一章和若干个小节,结构为里面包含2个td，第一个是章节名称，第二个是小节序号和链接
+    all_tr_tag: ResultSet = soup.find_all('tr')
+    for tr_item in all_tr_tag:
+        # 生命单一章节数据结构
+        one_chapter_dict: dict = {}
+        paragraph_links_list: List[Dict[str, str]] = []
+        # one_paragraph_dict: Dict[str, str] = {}
+        # 获取单一章节数据
+        all_td_tag_in_tr_item: ResultSet = tr_item.find_all('td')
+        # 获取每章标题
+        chapter_name_td_tag: ResultSet = all_td_tag_in_tr_item[0]
+        chapter_title: str = chapter_name_td_tag.text
+        one_chapter_dict['chapter_title'] = chapter_title
+        # 包含所有小节a标签的td列表
+        all_paragraph_td_tag: ResultSet = all_td_tag_in_tr_item[1]
+        for paragraph_td_tag in all_paragraph_td_tag.find_all('a'):
+            one_paragraph_dict: Dict[str, str] = {}
+            one_paragraph_order: str = paragraph_td_tag.text
+            one_paragraph_link: str = 'http://www.godcom.net/niv/' + paragraph_td_tag.get('href')
+            # print(one_paragraph_link)
+            one_paragraph_dict['order']=one_paragraph_order
+            one_paragraph_dict['link'] = one_paragraph_link
+            paragraph_links_list.append(one_paragraph_dict)
+        one_chapter_dict['paragraph_links_list'] = paragraph_links_list
+        Bible_content_structure.append(one_chapter_dict)
+
+    return Bible_content_structure
+
+
+def write_content_to_docx(file_name:str='NIV Bible',path: str = '.') -> None:
+    document = Document()
+    path_and_name:str=path+'/'+file_name
+    Bible_content_structure:List[Dict] = get_Bible_chapters()
+    for item in Bible_content_structure:
+        chapter_title:str=item['chapter_title']
+        document.add_heading(chapter_title)
+        for paragraph in item['paragraph_links_list']:
+            paragraph_order=paragraph['order']
+            paragraph_link = paragraph['link']
+            document.add_heading(paragraph_order,level=2)
+            document.add_paragraph
+            print(paragraph)
+    document.save(path_and_name)
+def get_paragraph_content(paragraph_url:str='http://www.godcom.net/niv/B01C001.htm')->str:
+    r=requests.get(paragraph_url).text
+    print(r)
+
+if __name__ == '__main__':
+    # write_content_to_docx()
+    get_paragraph_content()
